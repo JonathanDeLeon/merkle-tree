4c3de6fdd51445edbb6f301f89cc69deace1ba0e
Jonathan De Leon <jrdeleon@usc.edu>
2021-03-17 22:12:17 -0700

    Stub out methods

---
+++ buildmtree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Jonathan De Leon
+# CSCI 531 Applied Cryptography
+# March, 2021
+
+class MerkleTree:
+
+    def __init__(self):
+        self.hashList = []
+
+    def generateTree(self):
+        return []
+
+
+if __name__ == "__main__":
+    print("Welcome to Merkle Tree")
